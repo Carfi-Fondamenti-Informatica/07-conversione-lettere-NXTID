7a4b2713e6a2b04a2ac23e71abe98e14017be938
NXTID <117934144+NXTID@users.noreply.github.com>
2022-12-08 18:13:10 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+bool conversione(char &a) {
+    int b = 0;
+    
+    if (a >= 97 && a <= 122 or a >= 65 && a <= 90){
+        if (a >= 97 && a <= 122) {
+            b = a;
+            b=b-97;
+            a = 65 + b;
+        } else if (a >= 65 && a <= 90) {
+            b = a;
+            b=b-65;
+            a = 97 + b;
+        }
+    return true;
+    }else
+        return false;
+}
