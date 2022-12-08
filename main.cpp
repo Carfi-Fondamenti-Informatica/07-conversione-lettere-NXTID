1a7cef3f93f7b3363b03ab044dabf20add40b4a4
NXTID <117934144+NXTID@users.noreply.github.com>
2022-12-08 18:13:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char a=0;
+
+    cin>>a;
+    if(conversione (a)==true)
+        cout<<a;
+    else
+        cout<<"errore";
+
+    return 0;
 }
